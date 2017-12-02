90d1b233e7bfdfba752d1a3d7e17dc04bb1a6f37
YanFangyao <yanfangyao01@lianjia.com>
2017-12-02 13:53:21 +0800

    add : 分类页

--- src/pages/category/index.js
+++ src/pages/category/index.js
@@ -10,47 +10,86 @@ import typelist from '../typelist'
 import tpl from './tpl.vtpl'
 
 export default Vue.component('category', {
-  props: {
-    listpadding: {
-      type: String,
-      default: '0'
+    data() {
+        return {
+            icon: 'icon',
+            list: [{
+                    title: '购房',
+                    subitem: [{
+                            jumplink: '/lvoice/category',
+                            iconName: 'icon-coin',
+                            subtitle: '贷款'
+                        },
+                        {
+                            jumplink: '/lvoice/category',
+                            iconName: 'icon-coin',
+                            subtitle: '置业'
+                        },
+                        {
+                            jumplink: '/lvoice/category',
+                            iconName: 'icon-coin',
+                            subtitle: '投资'
+                        }, {
+                            jumplink: '/lvoice/category',
+                            iconName: 'icon-coin',
+                            subtitle: '看房'
+                        }
+                    ]
+                },
+                {
+                    title: '装修',
+                    subitem: [{
+                            jumplink: '/lvoice/category',
+                            iconName: 'icon-coin',
+                            subtitle: '贷款'
+                        },
+                        {
+                            jumplink: '/lvoice/category',
+                            iconName: 'icon-coin',
+                            subtitle: '置业'
+                        },
+                        {
+                            jumplink: '/lvoice/category',
+                            iconName: 'icon-coin',
+                            subtitle: '投资'
+                        }, {
+                            jumplink: '/lvoice/category',
+                            iconName: 'icon-coin',
+                            subtitle: '看房'
+                        }
+                    ]
+                },
+                {
+                    title: '特色',
+                    subitem: [{
+                            jumplink: '/lvoice/category',
+                            iconName: 'icon-coin',
+                            subtitle: '贷款'
+                        },
+                        {
+                            jumplink: '/lvoice/category',
+                            iconName: 'icon-coin',
+                            subtitle: '置业'
+                        },
+                        {
+                            jumplink: '/lvoice/category',
+                            iconName: 'icon-coin',
+                            subtitle: '投资'
+                        }, {
+                            jumplink: '/lvoice/category',
+                            iconName: 'icon-coin',
+                            subtitle: '看房'
+                        }
+                    ]
+                }
+            ]
+        }
     },
-    imagesrc: {
-      type: String
+    computed: {
+        formartTopRight() {
+            const numberInfo = Number(this.toprighttitle)
+            return numberInfo > 10000 ? `${Math.floor(numberInfo / 10000)}万` : numberInfo
+        }
     },
-    showtoprighttips: {
-      type: Boolean,
-      default: true
-    },
-    toprighticonclass: {
-      type: String
-    },
-    toprighttitle: {
-      type: String
-    },
-    showbottomtips: {
-      type: Boolean,
-      default: true
-    },
-    bottomtips: {
-      type: String
-    },
-    showbottomtitle: {
-      type: Boolean,
-      default: true
-    },
-    bottomtitle: {
-      type: String
-    },
-    listwidth: {
-      type: String
-    }
-  },
-  computed: {
-    formartTopRight () {
-      const numberInfo = Number(this.toprighttitle)
-      return numberInfo > 10000 ? `${Math.floor(numberInfo / 10000)}万` : numberInfo
-    }
-  },
-  template: tpl
+    template: tpl
 })
