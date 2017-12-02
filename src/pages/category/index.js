b1a7109e2e5144c6f7159b3711bee74ba16487bd
YanFangyao <yanfangyao01@lianjia.com>
2017-12-02 11:12:52 +0800

    modify: rem布局

---
+++ src/pages/category/index.js
@@ -0,0 +1,56 @@
+/**
+ * @file: 分类页
+ * @author: yanfangyao01@lianjia.com
+ */
+import Vue from 'vue'
+import './style.less'
+// import musicsheet from '../musicsheet/musicsheet.vue'
+import typelist from '../typelist'
+// import menulist from '../menulist'
+import tpl from './tpl.vtpl'
+
+export default Vue.component('category', {
+  props: {
+    listpadding: {
+      type: String,
+      default: '0'
+    },
+    imagesrc: {
+      type: String
+    },
+    showtoprighttips: {
+      type: Boolean,
+      default: true
+    },
+    toprighticonclass: {
+      type: String
+    },
+    toprighttitle: {
+      type: String
+    },
+    showbottomtips: {
+      type: Boolean,
+      default: true
+    },
+    bottomtips: {
+      type: String
+    },
+    showbottomtitle: {
+      type: Boolean,
+      default: true
+    },
+    bottomtitle: {
+      type: String
+    },
+    listwidth: {
+      type: String
+    }
+  },
+  computed: {
+    formartTopRight () {
+      const numberInfo = Number(this.toprighttitle)
+      return numberInfo > 10000 ? `${Math.floor(numberInfo / 10000)}万` : numberInfo
+    }
+  },
+  template: tpl
+})
