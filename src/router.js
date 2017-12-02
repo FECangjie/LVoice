95e7996abe1fd83123e545d5d37906e8aa75aea8
YanFangyao <yanfangyao01@lianjia.com>
2017-12-02 17:45:45 +0800

    conflict: 解决router冲突

--- src/router.js
+++ src/router.js
@@ -6,53 +6,47 @@ import Recommend from './pages/home/recommend'
 import category from './pages/home/category'
 import listPage from './pages/list'
 import ErrorPage from './pages/error'
-import category from './pages/category'
 import nice from './pages/nice'
 
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 export const router = new VueRouter({
-  mode: 'history',
-  base: __dirname,
-  routes: [
-  		{
-          path:'/',
-          component: homePage,
-          redirect: '/lvoice/recommend'
-      },
-      {
-          path:'/lvoice',
-          component: homePage,
-          children: [{
-            path: '',
+    mode: 'history',
+    base: __dirname,
+    routes: [{
+            path: '/',
+            component: homePage,
             redirect: '/lvoice/recommend'
-            },
-            {
-              path: '/lvoice/recommend',
-              component: Recommend
-            },
-            {
-              path: '/lvoice/category',
-              component: category
-            }
-        ]
-      },
-      {
-          path:'/list',
-          component: listPage
-      },
-      {
-          path:'/error',
-          component: ErrorPage
-      },
-      {
-          path: '/lvoice/category',
-          component: category
-      },
-      {
-          path: '/lvoice/nice',
-          component: nice
-      }
-  ]
+        },
+        {
+            path: '/lvoice',
+            component: homePage,
+            children: [{
+                    path: '',
+                    redirect: '/lvoice/recommend'
+                },
+                {
+                    path: '/lvoice/recommend',
+                    component: Recommend
+                },
+                {
+                    path: '/lvoice/category',
+                    component: category
+                }
+            ]
+        },
+        {
+            path: '/list',
+            component: listPage
+        },
+        {
+            path: '/error',
+            component: ErrorPage
+        },
+        {
+            path: '/lvoice/nice',
+            component: nice
+        }
+    ]
 })
