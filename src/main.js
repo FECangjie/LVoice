c93bb419df9fe500d0918b9423539867fbec6f80
QinChao <qinchao@lianjia.com>
2017-12-02 14:12:49 +0800

    build

--- src/main.js
+++ src/main.js
@@ -14,6 +14,53 @@ import 'mint-ui/lib/style.css'
 import './assets/styles/reset.css'
 import './assets/styles/index.css'
 
+(function(win, doc) {
+    var isIOS = win.navigator.appVersion.match(/iphone/gi);
+    var isAndorid = win.navigator.appVersion.match(/android/gi);
+    var deviceDpr = win.devicePixelRatio;
+    var setDpr = 1;
+    var scale = 1;
+    var rootEle = doc.documentElement;
+    var viewport = doc.querySelector('meta[name="viewport"]');
+
+    // 只针对ios进行处理
+    if (isIOS) {
+        if (deviceDpr >= 3) {
+            setDpr = 3;
+        } else if (deviceDpr >= 2) {
+            setDpr = 2;
+        } else {
+            setDpr = 1;
+        }
+    } else {
+        // 对安卓及其他设备设置dpr为1
+        setDpr = 1;
+    }
+    scale = 1 / setDpr;
+
+    if (!viewport) {
+        viewport = doc.createElement('meta');
+        viewport.setAttribute('name', 'viewport');
+        viewport.setAttribute('content', 'initial-scale=' + scale + ', maximum-scale=' + scale + ', minimum-scale=' + scale + ', user-scalable=' + 'no, uc-fitscreen=yes');
+
+        if (!rootEle.firstElementChild) {
+            var wrapper = doc.createElement('div');
+            wrapper.appendChild(viewport);
+            doc.write(wrapper.innerHTML);
+        } else {
+            rootEle.firstElementChild.appendChild(viewport);
+        }
+
+    } else {
+        // 如果页面已经设置了viewport，则对viewport进行更改
+        viewport.setAttribute('content', 'initial-scale=' + scale + ', maximum-scale=' + scale + ', minimum-scale=' + scale + ', user-scalable=no, uc-fitscreen=yes');
+    }
+    rootEle.setAttribute('style', 'font-size: ' + 16 * setDpr + 'px;');
+
+    window.DPR = setDpr;
+
+})(window, window.document);
+
 window.$http = Axios.create({
   baseURL: '/',
   timeout: 25000
