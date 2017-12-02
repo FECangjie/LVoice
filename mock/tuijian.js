9604bbf8bdf5fe2960421b1f664cb214441bc6d3
QinChao <qinchao@lianjia.com>
2017-12-02 12:28:50 +0800

    轮播图播放

---
+++ mock/tuijian.js
@@ -0,0 +1,50 @@
+module.exports = function(path, param, context) {
+    var result = {
+        "top_list": [{
+            "biaoqian_cn": "顶部图片中文1",
+            "tupian_url": "http://fdfs.xmcdn.com/group7/M0A/EE/85/wKgDX1agTp7BXacPAAA6YH5lMbo448.jpg",
+            "tag_color": "#B3EE3A",
+            "tag_cn": "标签中文",
+            "click_count": "123",
+            "click_url": "http://www.ximalaya.com/30249492/album/7060646/"
+        }, {
+            "biaoqian_cn": "顶部图片中文2",
+            "tupian_url": "http://fdfs.xmcdn.com/group7/M0A/EE/85/wKgDX1agTp7BXacPAAA6YH5lMbo448.jpg",
+            "tag_color": "#B3EE3A",
+            "tag_cn": "标签中文",
+            "click_count": "123",
+            "click_url": "http://www.ximalaya.com/30249492/album/7060646/"
+        }],
+        "jiugongge_list": [{
+            "biaoqian_cn": "九宫格图片中文1",
+            "tupian_url": "http://fdfs.xmcdn.com/group7/M0A/EE/85/wKgDX1agTp7BXacPAAA6YH5lMbo448.jpg",
+            "tag_color": "#B3EE3A",
+            "tag_cn": "标签中文",
+            "click_count": "123",
+            "click_url": "http://www.ximalaya.com/30249492/album/7060646/"
+        }, {
+            "biaoqian_cn": "九宫格图片中文2",
+            "tupian_url": "http://fdfs.xmcdn.com/group7/M0A/EE/85/wKgDX1agTp7BXacPAAA6YH5lMbo448.jpg",
+            "tag_color": "#B3EE3A",
+            "tag_cn": "标签中文",
+            "click_count": "123",
+            "click_url": "http://www.ximalaya.com/30249492/album/7060646/"
+        }],
+        "guest_list": [{
+            "biaoqian_cn": "猜你喜欢图片中文1",
+            "tupian_url": "http://fdfs.xmcdn.com/group7/M0A/EE/85/wKgDX1agTp7BXacPAAA6YH5lMbo448.jpg",
+            "tag_color": "#B3EE3A",
+            "tag_cn": "标签中文",
+            "click_count": "123",
+            "click_url": "http://www.ximalaya.com/30249492/album/7060646/"
+        }, {
+            "biaoqian_cn": "猜你喜欢图片中文2",
+            "tupian_url": "http://fdfs.xmcdn.com/group7/M0A/EE/85/wKgDX1agTp7BXacPAAA6YH5lMbo448.jpg",
+            "tag_color": "#B3EE3A",
+            "tag_cn": "标签中文",
+            "click_count": "123",
+            "click_url": "http://www.ximalaya.com/30249492/album/7060646/"
+        }]
+    }
+    return result
+}
--- src/pages/home/recommend/index.js
+++ src/pages/home/recommend/index.js
@@ -13,50 +13,52 @@ import list from 'pages/list'
 
 export default Vue.component('home', {
   data () {
-      return {
-    swiperOption: {
-      autoplay: 1113000,
-      grabCursor: true,
-      setWrapperSize: true,
-      autoHeight: true,
-      pagination: '.swiper-pagination',
-      paginationClickable: true,
-      mousewheelControl: true,
-      autoplayDisableOnInteraction: false,
-      observeParents: true,
-      loop: true,
-      onSlideChangeEnd: swiper => {
-        // 这个位置放swiper的回调方法
-        this.page = swiper.realIndex + 1
-        this.index = swiper.realIndex
+    return {
+      swiperOption: {  // a轮播图配置项
+        autoplay: 1113000,
+        grabCursor: true,
+        setWrapperSize: true,
+        autoHeight: true,
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        mousewheelControl: true,
+        autoplayDisableOnInteraction: false,
+        observeParents: true,
+        loop: true,
+        onSlideChangeEnd: swiper => {
+          // 这个位置放swiper的回调方法
+          this.page = swiper.realIndex + 1
+          this.index = swiper.realIndex
+        },
+        onClick: swiper => {
+          debugger
+          const index = swiper.activeIndex
+          store.commit({
+            type: 'setMusicList',
+            list: JSON.parse(swiper.slides[index].getAttribute('songlist'))
+          })
+          store.commit({
+            type: 'playIndex',
+            index: 0
+          })
+          store.dispatch({
+            type: 'set_MusicDetail',
+            isShow: true
+          })
+        }
       },
-      onClick: swiper => {
-        const index = swiper.activeIndex
-        store.commit({
-          type: 'setMusicList',
-          list: JSON.parse(swiper.slides[index].getAttribute('songlist'))
-        })
-        store.commit({
-          type: 'playIndex',
-          index: 0
-        })
-        store.dispatch({
-          type: 'set_MusicDetail',
-          isShow: true
-        })
-      }
+      swiperIndex: 0
     }
-      }
+  },
+  computed: {
+    reconmmend () {
+      return store.getters.getAllInfo || '暂无数据'
     },
-    computed: {
-      reconmmend () {
-        return store.getters.getAllInfo || '暂无数据'
-      },
-      swiper () {
-        return this.$refs.mySwiper.swiper
-      }
-    },
-    methods: {
+    swiper () {
+      return this.$refs.mySwiper.swiper
+    }
+  },
+  methods: {
     showSongSheet (data) {
       store.dispatch({
         type: 'set_MusicSheetList',
@@ -66,8 +68,30 @@ export default Vue.component('home', {
         type: 'setIsShowSongSheet',
         isShow: true
       })
-    }
     },
+    swiperClick(e) {
+      const index = this.swiperIndex
+      let target = e.target
+      let swiper = $(target).parent()
+      debugger
+
+      store.commit({
+        type: 'setMusicList',
+        list: JSON.parse(swiper[0].getAttribute('songlist'))
+      })
+      store.commit({
+        type: 'playIndex',
+        index: 0
+      })
+      store.dispatch({
+        type: 'set_MusicDetail',
+        isShow: true
+      })
+    },
+    swiperChange(index) {
+      this.swiperIndex = index
+    }
+  },
   mounted () {
     // this.$refs.mySwiper.slideTo(0, 1000, false)
   },
